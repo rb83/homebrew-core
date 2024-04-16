867ccea978f3729f49f676106486c5ef387eec96
Michael Cho <michael@michaelcho.dev>
2024-04-16 17:28:01 -0400

    gource: revision bump (boost 1.85.0)

--- Formula/g/gource.rb
+++ Formula/g/gource.rb
@@ -4,7 +4,7 @@ class Gource < Formula
   url "https://github.com/acaudwell/Gource/releases/download/gource-0.54/gource-0.54.tar.gz"
   sha256 "1dcbcedf65d2cf4d69fe0b633e54c202926c08b829bcad0b73eaf9e29cd6fae5"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
 
   bottle do
     sha256 arm64_sonoma:   "b8b897102f246e11f945323cef9e073719f726e4d954a43705453fbe26d876f7"
